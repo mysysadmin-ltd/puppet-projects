899d151d444c17a5186e443e896e6c96997fb774
Dan Foster <dan.foster@bristol.ac.uk>
2015-02-25 16:35:42 +0000

    add apache project defs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,34 @@
 # == Class: projects
 #
-# Full description of class projects here.
+# A puppet module to manage top level projects.
 #
 # === Examples
 #
-#  class { 'projects':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
 # === Authors
 #
-# Author Name <author@domain.com>
+# Dan Foster <dan@zem.org.uk>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Dan Foster, unless otherwise noted.
 #
 class projects (
 ) inherits ::projects::params {
 
+  $projects = hiera_hash('projects',{})
+  create_resources('project', $projects)
 
+}
+
+# == Resource type: project
+#
+# A top level project type.
+define project (
+  $apache = {}
+) {
+
+  # Create apache vhosts
+  if defined(Class['apache']) {
+    create_resources('apache::vhost', $apache)
+  }
 }
