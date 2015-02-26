0bb3414ca20038e5883ef997b7f363d6ef0f55e1
Dan Foster <dan.foster@bristol.ac.uk>
2015-02-26 09:27:05 +0000

    Add base dir, user and group for project

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,22 +13,54 @@
 # Copyright 2015 Dan Foster, unless otherwise noted.
 #
 class projects (
+  basedir = '/srv/projects'
 ) inherits ::projects::params {
 
+  file { $basedir:
+    ensure => directory,
+    mode   => 0775,
+    owner  => root,
+    group  => root
+  }
+
   $projects = hiera_hash('projects',{})
-  create_resources('project', $projects)
+  create_resources('projects::project', $projects)
+
 
 }
 
 # == Resource type: project
 #
 # A top level project type.
-define project (
-  $apache = {}
+define projects::project (
+  $apache = {},
+  $uid = undef,
+  $gid = undef,
+  $comment = ""
 ) {
+
+  # If least one project definition exists for this host, creaste the base structure
+  if ($apache != {}) {
+    user { $title:
+      comment => $comment,
+      uid     => $uid,
+      gid     => $gid,
+      home    => '$basedir/$title'
+    }
+
+    group { $title:
+      comment => $comment,
+      gid     => $gid,
+    }
+
+    file { '$basedir/$title':
+      ensure => directory
+    }
+  }
 
   # Create apache vhosts
   if defined(Class['apache']) {
     create_resources('apache::vhost', $apache)
   }
 }
+
