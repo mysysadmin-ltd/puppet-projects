053e46a1fdcee5b5128782671efe1bb61e188208
Dan Foster <dan.foster@bristol.ac.uk>
2015-03-04 14:31:22 +0000

    Add apache vhost project type

--- manifests/init.pp
+++ manifests/init.pp
@@ -79,22 +79,51 @@ define projects::project::apache (
   $vhosts = {}
 ) {
 
-    file { "$::projects::basedir/$title/var/www":
-      ensure  => directory,
-      owner   => $title,
-      group   => $title,
-      require => File["$::projects::basedir/$title/var"],
-    }
+  file { "$::projects::basedir/$title/var/www":
+    ensure  => directory,
+    owner   => $title,
+    group   => $title,
+    require => File["$::projects::basedir/$title/var"],
+  }
 
-    file { "$::projects::basedir/$title/etc/apache":
-      ensure  => directory,
-      owner   => $title,
-      group   => $title,
-      require => File["$::projects::basedir/$title/etc"],
-    }
+  file { "$::projects::basedir/$title/etc/apache":
+    ensure  => directory,
+    owner   => $title,
+    group   => $title,
+    require => File["$::projects::basedir/$title/etc"],
+  }
 
-#    each($vhosts) |$k,$v| {
-#    }
-    create_resources('apache::vhost', $vhosts)
+  file { "$::projects::basedir/$title/etc/apache/conf.d":
+    ensure  => directory,
+    owner   => $title,
+    group   => $title,
+    require => File["$::projects::basedir/$title/etc/apache"],
+  }
+
+  create_resources('::projects::project::apache::vhost', $vhosts, {
+    'projectname' => $title
+  })
+  create_resources('::apache::vhost', $vhosts)
+
 }
 
+
+# -- Reoutce type: project::apache::vhost
+#
+# Configures and projec apache vhost.
+define projects::project::apache::vhost (
+  $projectname = undef,
+  $docroot = undef,
+  $port = undef,
+  $vhost_name = undef,
+  $ssl = false
+) {
+
+  file { "$::projects::basedir/$projectname/etc/apache/conf.d/$title":
+    ensure  => directory,
+    owner   => $projectname,
+    group   => $projectname,
+    require => File["$::projects::basedir/$projectname/etc/apache/conf.d"],
+  }
+
+}
