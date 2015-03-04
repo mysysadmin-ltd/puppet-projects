4ffe5bc4a30abda3df7f9d84360b1fb78ada5e94
Dan Foster <dan.foster@bristol.ac.uk>
2015-03-04 08:56:17 +0000

    Add ::projects::project::apache

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,8 +23,8 @@ class projects (
     group  => root
   }
 
-  $projects = hiera_hash('projects',{})
-  create_resources('projects::project', $projects)
+  $projects = hiera_hash('::projects',{})
+  create_resources('::projects::project', $projects)
 
 
 }
@@ -58,11 +58,50 @@ define projects::project (
       owner  => $title,
       group  => $title
     }
+
+    file { "$::projects::basedir/$title/var":
+      ensure => directory,
+      owner  => $title,
+      group  => $title
+    }
+
+    file { "$::projects::basedir/$title/etc":
+      ensure => directory,
+      owner  => $title,
+      group  => $title
+    }
   }
 
   # Create apache vhosts
   if defined(Class['apache']) {
-    create_resources('apache::vhost', $apache)
+    projects::project::apache { $name:
+      vhosts = $apache
+    }
   }
+}
+
+# -- Resource type: project::apache
+#
+# Defines an apache project
+define projects::project::apache (
+  $vhosts = {}
+) {
+
+    file { "$::projects::basedir/$projectnamei/var/www":
+      ensure => directory,
+      owner  => $title,
+      group  => $title
+    }
+
+    file { "$::projects::basedir/$projectnamei/etc/apache":
+      ensure => directory,
+      owner  => $title,
+      group  => $title
+    }
+
+    each($vhosts) |$k,$v| 
+    {
+    }
+    create_resources('apache::vhost', $vhosts)
 }
 
