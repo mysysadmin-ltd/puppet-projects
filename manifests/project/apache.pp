6715187dbaaecfe37383c46783dd508aba2c97ac
Dan Foster <dan.foster@bristol.ac.uk>
2015-04-13 17:29:44 +0100

    Add apache and tomcat configuration in to profile (removing from role)

--- manifests/project/apache.pp
+++ manifests/project/apache.pp
@@ -90,8 +90,8 @@ define projects::project::apache::vhost (
     additional_includes => ["$::projects::basedir/$projectname/etc/apache/conf.d/","$::projects::basedir/$projectname/etc/apache/conf.d/$title/"]
   }
 
-  if !defined(Firewall['050 accept Apache $port']) {
-    firewall { '050 accept Apache $port':
+  if !defined(Firewall["050 accept Apache $port"]) {
+    firewall { "050 accept Apache $port":
       port   => $port,
       proto  => tcp,
       action => accept,
