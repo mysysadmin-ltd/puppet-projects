ca537e138a25a27cfb6bf04f30dbf6654be25718
Dan Foster <dan.foster@bristol.ac.uk>
2015-07-10 11:32:36 +0100

    Add php-mysql

--- manifests/project/apache.pp
+++ manifests/project/apache.pp
@@ -26,7 +26,7 @@ define projects::project::apache (
   $php = hiera('projects::apache_common::php',false)
   if $php == true {
     ensure_resource('class', '::apache::mod::php', {})
-    ensure_packages(['php-pdo'])
+    ensure_packages(['php-pdo', 'php-mysql'])
   }
 
 
