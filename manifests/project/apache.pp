90d9d67220c8868a73da9bbe5dfbc9e4a1e34317
David Goodall <david@mysysadmin.co.uk>
2016-09-23 17:09:48 +0100

    Backing out change to wrong branch

--- manifests/project/apache.pp
+++ manifests/project/apache.pp
@@ -185,7 +185,6 @@ CustomLog \"${::projects::basedir}/${projectname}/var/log/httpd/${title}_access.
     ssl_pkey { "${::projects::basedir}/${projectname}/etc/ssl/private/${vhost_name}.auto.key" :
       ensure   => present,
       require  => File["${::projects::basedir}/${projectname}/etc/ssl/private"],
-      before => Class['::apache']",
     }
 
     x509_request { "${::projects::basedir}/${projectname}/etc/ssl/csrs/${vhost_name}.auto.csr" :
